5d9c43a32046231afae053791bb638b0ed26f8b8
HBarbier <hbarbier@artfx.fr>
2020-10-07 16:58:49 +0200

    player speed +, start wall jump and triple jump, smooth jump

--- GAME3_BARBIER_Hugo_PROJECT/Assets/Scripts/PlayerController.cs
+++ GAME3_BARBIER_Hugo_PROJECT/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,11 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
 
     private Vector3 moveDirection;
+    public float jump2;
+
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool isJumping;
 
     // Start is called before the first frame update
     void Start()
@@ -26,25 +31,59 @@ public class PlayerController : MonoBehaviour
 
         moveDirection.y = yStore;
 
+
+        //JUMP
         if (controller.isGrounded)
         {
             moveDirection.y = 0f;
-            if (Input.GetButtonDown("Jump"))
+            jump2 += Time.deltaTime;
+            
+            if (Input.GetButtonDown("Jump") && jump2 > 0.3)
+            {
+                isJumping = true;
+                jumpTimeCounter = jumpTime;
+                moveDirection.y = jumpForce;
+                jump2 = 0;
+            }
+
+            if (Input.GetButtonDown("Jump") && jump2 <= 0.3)
+            {
+                isJumping = true;
+                jumpTimeCounter = jumpTime;
+                moveDirection.y = jumpForce + 5;
+                jump2 = 0;
+            }
+        }
+
+        if (isJumping == true && Input.GetKey(KeyCode.Space))
+        {
+            if (jumpTimeCounter > 0)
             {
                 moveDirection.y = jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
             }
+
+            else if (jumpTimeCounter < 0)
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
         }
 
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
     }
 
-    //Wall jump
+    //WALL JUMP
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (!controller.isGrounded && hit.normal.y < 0.1f)
-        { 
-            if(Input.GetButtonDown("Jump"))
+        {
+            if (Input.GetButtonDown("Jump"))
             {
                 moveDirection.y = jumpForce;
             }
